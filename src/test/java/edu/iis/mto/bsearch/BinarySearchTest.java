8559cbdf8ee36afc3b07b6c3ea987d787cd4a013
grzegorz kabzinski <grzesiek.kab@gmail.com>
2019-03-26 10:49:36 +0100

    testy_v2 changed all functions you use assertThat method

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -3,50 +3,48 @@ package edu.iis.mto.bsearch;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class BinarySearchTest {
     @Test
     public void testIfElementIsInSequence(){
         int[] sequence = {1};
-        Assert.assertTrue(BinarySearch.search(1, sequence).isFound());
-    }
+        Assert.assertThat(true, is(equalTo(BinarySearch.search(1, sequence).isFound())));    }
 
     @Test
     public void testIfElementIsNotInSequence(){
         int[] sequence = {1};
-        Assert.assertFalse(BinarySearch.search(2, sequence).isFound());
+        Assert.assertThat(false,is(equalTo((BinarySearch.search(2, sequence).isFound()))));
     }
 
     @Test
     public void elementFirstInSequenceTest(){
         int[] sequence = {1,2,3,4};
-        Assert.assertEquals(0,BinarySearch.search(1,sequence).getPosition());
+        Assert.assertThat(1, is(equalTo(sequence[BinarySearch.search(1, sequence).getPosition()])));
     }
 
     @Test
     public void elementLastInSequenceTest() {
         int[] sequence = {1,2,3,4};
-        Assert.assertEquals(sequence.length - 1, BinarySearch.search(4, sequence).getPosition());
+        Assert.assertThat(4, is(equalTo(sequence[BinarySearch.search(4, sequence).getPosition()])));
     }
 
     @Test
     public void elementMiddleInSequenceTest() {
         int[] sequence = {1,2,3,4,5};
-        Assert.assertEquals((sequence.length / 2), BinarySearch.search(3, sequence).getPosition());
-    }
+        Assert.assertThat(3, is(equalTo(sequence[BinarySearch.search(3, sequence).getPosition()])));    }
 
     @Test
     public void testIfElementIsNotInMultipleElementSequence(){
         int[] sequence = {1,2,3,4,5};
-        Assert.assertFalse(BinarySearch.search(6, sequence).isFound());
+        Assert.assertThat(-1, is(equalTo(BinarySearch.search(6, sequence).getPosition())));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testIfSequenceIsEmpty(){
         int[] sequence = {};
-        Assert.assertFalse(BinarySearch.search(1, sequence).isFound());
-        Assert.assertEquals(-1, BinarySearch.search(1,sequence).getPosition());
+        BinarySearch.search(1, sequence);
     }
 
 }
